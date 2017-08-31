f6c4897bde35de1e4273d5e0103d8dc86554e89a
churchwerks <steven.lennis@icloud.com>
2017-08-31 03:10:47 -0500

    Some Cats Games

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,30 +2,43 @@ module Players
   class Computer < Player
     #constant
       WIN_COMBO = [
-                        [6,4,2],
-                        [3,4,5],
+                        [0,4,8],
                         [1,4,7],
-                        [0,4,8]]
+                        [3,4,5],
+                        [6,4,2]]
     def move(board)
       prng = Random.new
       user_input = prng.rand(9) + 1
       board.valid_move?(user_input) ? user_input.to_s : nil
     end
-
     def logical_move(board)
-      case board.turn_count.even?
-      when true
-      if board.turn_count <= 2
-        move(board)
-      elsif board.valid_move?("5")
-        user_input = "5"
+      if board.turn_count.even?
+        if board.valid_move?("5")
+          user_input = "5"
+        else
+          move(board)
+        end
       else
-        #binding.pry
-        move(board)
+        index = board.cells.rindex { |x| x == "X" }
+        if index == 4 && board.valid_move?("1")
+          user_input = "1"
+        elsif index == 7 && board.valid_move?("2")
+          user_input = "2"
+        elsif index == 5 && board.valid_move?("4")
+          user_input = "4"
+        elsif index == 2 && board.valid_move?("7")
+          user_input = "7"
+        elsif index == 6 && board.valid_move?("3")
+          user_input = "3"
+        elsif index == 3 && board.valid_move?("6")
+          user_input = "6"
+        elsif index == 1 && board.valid_move?("8")
+          user_input = "8"
+        else
+          #binding.pry
+          move(board)
+        end
       end
-      when false
-      move(board)
     end
   end
-end
 end
