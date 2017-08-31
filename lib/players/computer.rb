85cac28920d9481b0c66b59b40deed6cd0acc6b3
churchwerks <steven.lennis@icloud.com>
2017-08-30 22:40:00 -0500

    Totally Random Game

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,15 +2,30 @@ module Players
   class Computer < Player
     #constant
       WIN_COMBO = [
-                        [0,4,8],
-                        [1,4,7],
+                        [6,4,2],
                         [3,4,5],
-                        [6,4,2]]
+                        [1,4,7],
+                        [0,4,8]]
     def move(board)
       prng = Random.new
       user_input = prng.rand(9) + 1
-      #binding.pry
       board.valid_move?(user_input) ? user_input.to_s : nil
     end
+
+    def logical_move(board)
+      case board.turn_count.even?
+      when true
+      if board.turn_count <= 2
+        move(board)
+      elsif board.valid_move?("5")
+        user_input = "5"
+      else
+        #binding.pry
+        move(board)
+      end
+      when false
+      move(board)
+    end
   end
+end
 end
